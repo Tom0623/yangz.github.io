3664c0871e70e928dec8abc3b49ebe200c25c8ba
ZY <104945565+Tom0623@users.noreply.github.com>
2026-07-13 15:53:55 -0400

    the link to html

--- Lab4/Part 1/java/script.js
+++ Lab4/Part 1/java/script.js
@@ -29,7 +29,7 @@ function returnRandomStoryString() {
     const randomPlace = randomValueFromArray(places);
     const randomEvent = randomValueFromArray(events);
     // It was 94 Fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
-    const stories = " It was 94 Fahrenheit outside, so ${randomCharacter} went for a walk. When they got to ${randomPlace}, they stared in horror for a few moments, then ${randomEvent}. Bob saw the whole thing, but was not surprised — ${randomCharacter} weighs 300 pounds, and it was a hot day."
+    let storyText = `It was 94 Fahrenheit outside, so ${randomCharacter} went for a walk. When they got to ${randomPlace}, they stared in horror for a few moments, then ${randomEvent}. Bob saw the whole thing, but was not surprised — ${randomCharacter} weighs 300 pounds, and it was a hot day.`;
     return storyText;
 }
 
@@ -47,8 +47,8 @@ function generateStory() {
 
     if (document.getElementById("uk").checked) {
 
-        const weight = Math.round(300/14)+"stone";
-        const temperature = Math.round((94-32)*1.8)+"Celsius";
+        const weight = Math.round(300/14) + " stone";
+        const temperature = Math.round((94-32)/1.8) + " Celsius";
         newStory = newStory.replace("300 pounds",weight);
         newStory = newStory.replace("94 Fahrenheit",temperature);
     }
