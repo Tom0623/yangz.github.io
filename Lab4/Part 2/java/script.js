b806dcb5831541da33f82ff9fa079038ee996297
ZY <104945565+Tom0623@users.noreply.github.com>
2026-07-14 16:31:31 -0400

    Make an array for the images and add all image to this array

--- Lab4/Part 2/java/script.js
+++ Lab4/Part 2/java/script.js
@@ -3,3 +3,12 @@ const thumbBar = document.querySelector(".thumb-bar");
 
 const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
+
+// Make an array for the images and add all image to this.
+const images = [{filename: "images/pic1.jpg" , alt: "Closeup of a human eye"},
+    {filename: "images/pic2.jpg" , alt: "Rock that looks like a wave"},
+    {filename: "images/pic3.jpg" , alt: "Purple and white pansies"},
+    {filename: "images/pic4.jpg" , alt: "Section of wall from a pharaoh's tomb"},
+    {filename: "images/pic5.jpg" , alt: "Large moth on a leaf"}];
+
+//
