a52b4422582ab055d5390489943a1de15cdd1bf7
ZY <104945565+Tom0623@users.noreply.github.com>
2026-07-15 18:22:08 -0400

    Create a constant called baseURL

--- Lab4/Part 2/java/script.js
+++ Lab4/Part 2/java/script.js
@@ -11,4 +11,6 @@ const images = [{filename: "images/pic1.jpg" , alt: "Closeup of a human eye"},
     {filename: "images/pic4.jpg" , alt: "Section of wall from a pharaoh's tomb"},
     {filename: "images/pic5.jpg" , alt: "Large moth on a leaf"}];
 
-//
+// Add the images to the thumbnail bar
+// Create a constant called baseURL containing the base URL of each image file
+const baseURL = "https://github.com/Tom0623/yangz.github.io/tree/main/Lab4/Part%202/images";
