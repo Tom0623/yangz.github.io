5fed60e83f0de06c9490779bf419725062fa3db4
ZY <104945565+Tom0623@users.noreply.github.com>
2026-07-16 11:41:14 -0400

    For the baseURL if I use my image files from the github is no't work so use the link the mdn have

--- Lab4/Part 2/java/script.js
+++ Lab4/Part 2/java/script.js
@@ -5,16 +5,18 @@ const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
 
 // Make an array for the images and add all image to this.
-const images = [{filename: "images/pic1.jpg" , alt: "Closeup of a human eye"},
-    {filename: "images/pic2.jpg" , alt: "Rock that looks like a wave"},
-    {filename: "images/pic3.jpg" , alt: "Purple and white pansies"},
-    {filename: "images/pic4.jpg" , alt: "Section of wall from a pharaoh's tomb"},
-    {filename: "images/pic5.jpg" , alt: "Large moth on a leaf"}];
+const images = [
+    {filename: "pic1.jpg" , alt: "Closeup of a human eye"},
+    {filename: "pic2.jpg" , alt: "Rock that looks like a wave"},
+    {filename: "pic3.jpg" , alt: "Purple and white pansies"},
+    {filename: "pic4.jpg" , alt: "Section of wall from a pharaoh's tomb"},
+    {filename: "pic5.jpg" , alt: "Large moth on a leaf"}
+];
 
 // Add the images to the thumbnail bar
 // Create a constant called baseURL containing the base URL of each image file
-const baseURL = "https://github.com/Tom0623/yangz.github.io/tree/main/Lab4/Part%202/images";
-
+// The url only can use mdn the one add to GitHub is not work. const baseURL = "https://github.com/Tom0623/yangz.github.io/tree/main/Lab4/Part%202/images";
+const baseURL = "https://mdn.github.io/shared-assets/images/examples/learn/gallery/";
 // make a for loop
 for (const image of images) {
     //For each image, create a new <img> element.
@@ -23,11 +25,11 @@ for (const image of images) {
     newImg.src = `${baseURL}${image.filename}`;
     newImg.alt = image.alt;
     //Add another attribute to the <img> to make it focusable via the keyword.
-    newImage.tabIndex = 0;
+    newImg.tabIndex = 0;
     //Append the <img> to the thumbBar.
-    thumbBar.appendChild(newImage);
+    thumbBar.appendChild(newImg);
     // Add a click event handler to the <img> so that when it is clicked, a function called updateDisplayedImage() is run, which displays the clicked image at full size. You'll create this function later on.
-    newImage.addEventListener('click', updateDisplayedImage);
+    newImg.addEventListener('click', updateDisplayedImage);
 }
 
 //  Create the updateDisplayedImage() function
@@ -38,7 +40,7 @@ function updateDisplayedImage(event) {
 
 //Wire up the Darken/Lighten button
 //Add a click event handler to the <button> with an anonymous function set as the handler function.
-btn.addEventListener("click", function() {
+btn.addEventListener("click", () => {
    // Inside the function body, add a conditional structure that tests whether the <button> has a class set on it of dark or not.
     if (btn.classList.contains("dark")) {
         //If the <button> has a class of dark when clicked, change its text content to Lighten, and change the overlay element's background color to rgb(0 0 0 / 0.5). Remove the <button> element's dark class.
