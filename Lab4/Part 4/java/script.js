b4adfc785c57ea5713f14ad49da051b1d864a018
ZY <104945565+Tom0623@users.noreply.github.com>
2026-07-21 21:52:50 -0400

    Use the java file from part 3

--- Lab4/Part 4/java/script.js
+++ Lab4/Part 4/java/script.js
@@ -1,23 +1,20 @@
-// set up canvas
-
+// set the canvas
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
-// function to generate random number
-
+// Make a function
 function random(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// function to generate random RGB color value
-
 function randomRGB() {
-    return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+    return `rgb(${random(0, 255)} ${random(0, 255)} ${random(0, 255)})`;
 }
 
+// make a ball
 class Ball {
     constructor(x, y, velX, velY, color, size) {
         this.x = x;
@@ -27,38 +24,38 @@ class Ball {
         this.color = color;
         this.size = size;
     }
-
+    //draw the ball
     draw() {
         ctx.beginPath();
         ctx.fillStyle = this.color;
         ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
         ctx.fill();
     }
-
+    // update the ball
     update() {
         if (this.x + this.size >= width) {
-            this.velX = -Math.abs(this.velX);
+            this.velX = -this.velX;
         }
 
         if (this.x - this.size <= 0) {
-            this.velX = Math.abs(this.velX);
+            this.velX = -this.velX;
         }
 
         if (this.y + this.size >= height) {
-            this.velY = -Math.abs(this.velY);
+            this.velY = -this.velY;
         }
 
         if (this.y - this.size <= 0) {
-            this.velY = Math.abs(this.velY);
+            this.velY = -this.velY;
         }
 
         this.x += this.velX;
         this.y += this.velY;
     }
-
+    // Add Collision Detection
     collisionDetect() {
         for (const ball of balls) {
-            if (!(this === ball)) {
+            if (this !== ball) {
                 const dx = this.x - ball.x;
                 const dy = this.y - ball.y;
                 const distance = Math.sqrt(dx * dx + dy * dy);
@@ -71,6 +68,7 @@ class Ball {
     }
 }
 
+// Animated Ball
 const balls = [];
 
 while (balls.length < 25) {
@@ -83,19 +81,20 @@ while (balls.length < 25) {
         random(-7, 7),
         random(-7, 7),
         randomRGB(),
-        size
+        size,
     );
 
     balls.push(ball);
 }
-
+// make a function loop
 function loop() {
-    ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
+    ctx.fillStyle = "rgb(0 0 0 / 25%)";
     ctx.fillRect(0, 0, width, height);
 
     for (const ball of balls) {
         ball.draw();
         ball.update();
+        // Update your loop() function to call ball.collisionDetect() after ball.update()
         ball.collisionDetect();
     }
 
